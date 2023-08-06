b1a5fb39d66920d1291febeb4fb6fa64cf8d8833
EC2 Default User <ec2-user@ip-172-31-8-148.ap-northeast-1.compute.internal>
2023-08-06 07:20:55 +0000

    Fix view

--- app/controllers/public/bookmarks_controller.rb
+++ app/controllers/public/bookmarks_controller.rb
@@ -2,17 +2,15 @@ class Public::BookmarksController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    plan = Plan.find(params[:plan_id])
-    bookmark = current_user.bookmarks.new(plan_id: plan.id)
+    @plan = Plan.find(params[:plan_id])
+    bookmark = current_user.bookmarks.new(plan_id: @plan.id)
     bookmark.save
-    redirect_to request.referer
   end
 
   def destroy
-    plan = Plan.find(params[:plan_id])
-    bookmark = current_user.bookmarks.find_by(plan_id: plan.id)
+    @plan = Plan.find(params[:plan_id])
+    bookmark = current_user.bookmarks.find_by(plan_id: @plan.id)
     bookmark.destroy
-    redirect_to request.referer
   end
 
   def index
