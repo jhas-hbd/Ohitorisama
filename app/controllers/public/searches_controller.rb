af774887985cea9d117b1935459a24561cbf4026
EC2 Default User <ec2-user@ip-172-31-8-148.ap-northeast-1.compute.internal>
2023-08-07 07:39:55 +0000

    Fix sort_order

--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -4,7 +4,7 @@ class Public::SearchesController < ApplicationController
   def search
     @plans = Plan.all
     @plans = @plans.where(prefecture: plan_params[:prefecture]) if plan_params[:prefecture].present?
-    @planss = @plans.where(stay_days: plan_params[:stay_days]) if plan_params[:stay_days].present?
+    @plans = @plans.where(stay_days: plan_params[:stay_days]) if plan_params[:stay_days].present?
     @plans = @plans.where(budget: plan_params[:budget]) if plan_params[:budget].present?
     @plans = @plans.where(main_vehicle: plan_params[:main_vehicle]) if plan_params[:main_vehicle].present?
     @plans = @plans.where('title LIKE(?) or impression LIKE(?)',"%#{plan_params[:key_word]}%","%#{plan_params[:key_word]}%") if plan_params[:key_word].present?
@@ -13,7 +13,7 @@ class Public::SearchesController < ApplicationController
   def tag_search
     tags = Tag.where('name LIKE(?)',"%#{tag_params[:tag_name]}%") if tag_params[:tag_name].present?
     tags.each do |tag|
-      @plans = tag.plans
+      @plans = tag.plans.order(created_at: :desc)
     end
   end
 
--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -3,7 +3,7 @@ class Public::UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @plans = @user.plans
+    @plans = @user.plans.order(created_at: :desc)
   end
 
   def edit
