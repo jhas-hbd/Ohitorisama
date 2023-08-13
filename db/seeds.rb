f83ff56c17ac870b165869fb93c9fc72506d986f
EC2 Default User <ec2-user@ip-172-31-8-148.ap-northeast-1.compute.internal>
2023-08-13 06:27:17 +0000

    Fix seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+if Rails.env.development?
 
-Admin.create!(
-  email: 'admin@admin',
-  password: 'testtest'
-)
+  Admin.create!(
+    email: 'ad@min',
+    password: 'adminmin'
+  )
 
+end
